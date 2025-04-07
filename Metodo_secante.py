598e15e7f17ea3e9f9f493f8ec0054e65940e2ab
Horacio-Sutara <rhsuty@gmail.com>
2025-04-06 23:44:50 -0300

    optimizacion del metodo secante

--- Metodo_secante.py
+++ Metodo_secante.py
@@ -1,39 +1,41 @@
 import Ecuacion_procesar
+import numpy as np
 
 class MetodoSecante:
-    def __init__(self, x0,x1, error, ecuacion=None, iteracciones=50):
-        if ecuacion is None:
-            self.ecuacion = Ecuacion_procesar.Ecuacion_procesar()
-        else:
-            self.ecuacion = Ecuacion_procesar.Ecuacion_procesar(ecuacion)
-        self.x0 = x0
-        self.x1 = x1
+    def __init__(self, ecuacion, error=1e-7, iteracciones=50):
+        self.ecuacion = Ecuacion_procesar.Ecuacion_procesar(ecuacion)
         self.error_maximo = error
-        self.iteracciones=iteracciones
+        self.iteracciones = iteracciones
 
-    def calcular(self):
-        error=100
-        cont=0
-        while error>self.error_maximo:
-            if cont>self.iteracciones:
-                print("No se puede resolver")
-                return 0,False
-            cont+=1
-            try:
-                xn_1 = self.x1 - (self.ecuacion.resultado(self.x1)*(self.x1-self.x0))/(self.ecuacion.resultado(self.x1)-self.ecuacion.resultado(self.x0))
-            except:
-                print("Division por cero")
-                return 0,False
-            error=abs(xn_1-self.x1)
-            self.x0=self.x1
-            self.x1=xn_1
-        #print("Bucles: ",cont," Resultado: ",xn_1)
-        error=self.error_maximo
-        cont=0
-        while error<1:
-            error*=10
-            cont+=1
-        return self.ecuacion.truncar_sympy(xn_1,cont),True
+    def buscar_raiz(self, x0, x1):
+        try:
+            error = 100
+            cont = 0
+            while error > self.error_maximo and cont < self.iteracciones:
+                f_x0 = self.ecuacion.resultado(x0)
+                f_x1 = self.ecuacion.resultado(x1)
+
+                denominador = f_x1 - f_x0
+                if denominador == 0:
+                    return 0, False
+
+                xn_1 = x1 - (f_x1 * (x1 - x0)) / denominador
+                error = abs(xn_1 - x1)
+
+                x0, x1 = x1, xn_1
+                cont += 1
+
+            if cont >= self.iteracciones:
+                return 0, False
+
+            # Redondear según tolerancia
+            decimales = max(0, -int(round(np.log10(self.error_maximo))))
+            return self.ecuacion.truncar_sympy(xn_1, decimales), True
+
+        except Exception:
+            return 0, False
+
 if __name__=="__main__":
-    ecuacion=MetodoSecante(x0=-2,x1=-1.5,error=1e-7)
-    ecuacion.calcular()
+    ecuacion=MetodoSecante("x**2-2",error=1e-7)
+    res,band=ecuacion.buscar_raiz(x0=1,x1=2)
+    print(res)
