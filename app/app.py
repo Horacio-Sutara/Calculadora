e04a61a4fbd8364043d1e2f622bb9b3aa0af7b7d
Horacio-Sutara <rhsuty@gmail.com>
2025-04-16 15:20:11 -0300

    archivo de prueba html

--- app/app.py
+++ app/app.py
@@ -13,7 +13,7 @@ def index():
           'nombres':nombres,
           "numero_persona":len(nombres)} # se crea un diccionario con el contenido que se va a mostrar en la plantilla
     
-    return render_template('index.html',data=data) # render_template permite cargar archivos html
+    return render_template('prueba.html',data=data) # render_template permite cargar archivos html
 
 if __name__ == '__main__':
     app.run(debug=True,port=5000)# modo de desarrollo activado para ver errores y en que puerto esta visible
